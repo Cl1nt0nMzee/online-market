1710410c5c73c1d8d0ca6416c579c9816f6a13ba
Clinton Mzee <clinton.mzee@outlook.com>
2023-07-04 23:36:24 +0200

    Second Commit

---
+++ frontend/src/components/atoms/button/Button.jsx
@@ -0,0 +1,23 @@
+const PrimaryButton = () => {
+    return (
+        <button className="primary" type="button">
+        Primary Button
+        </button>
+    );
+}
+
+const SecondaryButton = () => {
+    return (
+        <button className="secondary" type="button">
+        Secondary Button
+        </button>
+    );
+}
+
+const TertiaryButton = () => {
+    return (
+        <button className="tertiary" type="button">
+        Tertiary Button
+        </button>
+    );
+}
