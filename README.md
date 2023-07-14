de4d9886135a69de6a3fd2d5d4e1ec2916819f45
Clinton Mzee <clinton.mzee@outlook.com>
2023-07-15 00:03:04 +0200

    update

--- README.md
+++ README.md
@@ -5,9 +5,16 @@
     Vite (Module Bundler)
     React (SPA)
     SASS (Styling)
+    Zustand (State Management)
     React Dom (Routing)
+
+### Starting Frontend
+
+    npm run dev     (Development)
+
+    npm run build   (Production)
 
 ## Backend
 
 
-# Installation
+## Installation
---
+++ test.js
@@ -0,0 +1,11 @@
+const pictures = {
+    login_picture: {    image_name: '',
+                        image_title: '',
+                        image_source: 'https://unsplash.com/photos/nY14Fs8pxT8',
+                        image_alt: 'purple and white heart illustration',
+                        image_owner: 'Milad Fakurian',
+                        image: LoginPicture},
+}
+
+let login_pic = pictures.login_picture
+console.log('Hi', login_pic.image_name)
