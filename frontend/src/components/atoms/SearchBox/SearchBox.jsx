f7e79907d8279752a1df16e1dbea16b944a703c3
Clinton Mzee <clinton.mzee@outlook.com>
2023-08-16 17:47:13 +0200

    fix(frontend): useRef returns null/undefined on Searchbox

--- frontend/src/components/atoms/SearchBox/SearchBox.jsx
+++ frontend/src/components/atoms/SearchBox/SearchBox.jsx
@@ -1,5 +1,36 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./_index.scss";
+
+const searchButton = ({}) => {
+  return (
+    <>
+      <button>search</button>
+    </>
+  );
+};
+const ClearButton = () => {
+  return (
+    <>
+      <button>clear</button>
+    </>
+  );
+};
+
+const searchInput = ({ placeholder, ref, onChangeFunction, id }) => {
+  return (
+    <>
+      <input
+      className="searchInput"
+        type="search"
+        name="Hello"
+        id={id}
+        onChangeFunction={onChangeFunction}
+        ref={ref}
+        placeholder={placeholder}
+      />
+    </>
+  );
+};
 
 export const SearchBox = () => {
   const searchItem = useRef();
@@ -7,25 +38,21 @@ export const SearchBox = () => {
 
   const handleChange = (e) => {
     e.preventDefault();
-    let val = e.target?.value;
-    setValue(val);
+    // let val = e.target?.value;
+    // setValue(val);
   };
+
   const clearInput = (e) => {
     e.preventDefault();
-    setValue("");
-    searchItem.current.value = "";
+    // setValue("");
+    // searchItem.current.value = "";
   };
-  
+
   return (
     <div className="searchBox">
-      <input
-        type="search"
-        placeholder="Search here"
-        ref={searchItem}
-        onChange={handleChange}
-      />
+      <searchInput placeholder={'lorem Ipsum'}/>
       <div className="buttonWrapper">
-        {searchItem.current.value && (<button onClick={clearInput}>clear</button>)}
+        {<button onClick={clearInput}>clear</button>}
         <button>search</button>
       </div>
     </div>
